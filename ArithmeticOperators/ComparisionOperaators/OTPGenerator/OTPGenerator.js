4c67ffcc5649922c4d2e784e6e8edf23b1ca0616
Likithadavala <likithadavala99@gmail.com>
2026-06-24 17:36:34 +0530

    OTP Generator

--- ArithmeticOperators/ComparisionOperaators/OTPGenerator/OTPGenerator.js
+++ ArithmeticOperators/ComparisionOperaators/OTPGenerator/OTPGenerator.js
@@ -1,15 +1,14 @@
 var OTP = 0;
 function onClickSendOTP() {
     debugger;
-   
     OTP = Math.trunc(Math.random() * 10 ** 6);
     console.log(OTP);
     alert(OTP);
 }
 function onClickAuthentication() {
     debugger;
-    let inputOTP = document.getElementById("txtInputOTP").value;
-    if (inputOTP == OTP) {
+    let inputOTP =Number(document.getElementById("txtInputOTP").value);
+    if (inputOTP === OTP) {
         document.getElementById("pAuthentication").innerHTML = "Successfully LogIn";
         document.getElementById("pAuthentication").style.color = "Green"
         document.getElementById("pAuthentication").style.padding = "30px";

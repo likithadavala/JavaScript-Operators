d351388e61812085f49c96b33a3c80bae302a61a
Likithadavala <likithadavala99@gmail.com>
2026-07-01 21:59:56 +0530

    Present or Absent using Notequalto

--- ArithmeticOperators/ComparisionOperaators/PresentOrAbsent/PresentOrAbsent.js
+++ ArithmeticOperators/ComparisionOperaators/PresentOrAbsent/PresentOrAbsent.js
@@ -1,8 +1,13 @@
 function onClickAttendances(){
     debugger;
     let name = document.getElementById("txtName").value.toLowerCase();
-    let isPresent = document.querySelector('input[name="isPresent"]:checked');
-    if(isPresent.value === "True"){
+    let isPresent = Number(document.querySelector('input[name="isPresent"]:checked').value);
+    console.log(name)
+    console.log(typeof name)
+    console.log(isPresent)
+    console.log(typeof isPresent)
+    console.log(isPresent != false)
+    if(isPresent != false){
         document.getElementById("pResult").innerHTML=`${name}  is Present Today`;
     } else {
         document.getElementById("pResult").innerHTML=`${name}  is Absent Today`;
